9e64388ecf77d6d43c95d373dd7413640720aebd
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2023-03-11 14:41:34 +0200

    fix bug

--- Graphics.py
+++ Graphics.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
-from Scraping import *
 import pandas as pd
+from bs4 import BeautifulSoup
 from PIL import Image
 import base64
 
@@ -10,6 +10,28 @@ def download_button(data):
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="data.csv">Download CSV</a>'
     return href
+
+def parse_listing(url, num_items):
+    data = {'name': [], 'url': [], 'price': [], 'rating': []}
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find_all('div', {'data-component-type': 's-search-result'})
+
+    for i in range(min(num_items, len(results))):
+        result = results[i]
+        try:
+            name = result.find('h2').get_text().strip()
+            url = "https://www.amazon.com" + result.find('a')['href']
+            price = result.find('span', {'class': 'a-offscreen'}).get_text().strip()
+            rating = result.find('span', {'class': 'a-icon-alt'}).get_text().strip()
+            data['name'].append(name)
+            data['url'].append(url)
+            data['price'].append(price)
+            data['rating'].append(rating)
+        except AttributeError:
+            continue
+    return data
+
 
 def graphics():
     st.write("# Amazon free web scraping tool")
@@ -25,8 +47,8 @@ def graphics():
     st.image(image, use_column_width=True)
 
     # Create a multi-choice menu
-    options = ["1", "5", "10", "all"]
-    selected_option = st.radio("Select the number of page to scrape:", options)
+    options = ["1", "5", "10", "100", "all"]
+    selected_option = st.radio("Select the number of items to scrape:", options)
 
     print(selected_option)
 
@@ -36,7 +58,7 @@ def graphics():
             if(string != "" and "amazon" in string):
                 st.write("Your link is: " + string)
                 #search_url = "https://www.amazon.com/s?k=bose&rh=n%3A12097479011&ref=nb_sb_noss"
-                data = parse_listing(string, selected_option)
+                data = parse_listing(string, int(selected_option))
                 df = pd.DataFrame(data)
                 print(df)
                 #df = df.drop(['price'], axis=1)
@@ -46,11 +68,3 @@ def graphics():
                 st.write("url not valid")
         except requests.exceptions.MissingSchema as e:
              st.write(f"Invalid URL: {e}")
-        except requests.exceptions.HTTPError as e:
-            st.write(f"The server returned an HTTP error: {e}")
-        except requests.exceptions.RequestException as e:
-             st.write(f"There was an error while making the request: {e}")
-        except Exception as e:
-            # handle any type of exception here
-            st.write(f"An error occurred: {e}")
-
